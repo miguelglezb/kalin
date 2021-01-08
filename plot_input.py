e07c41c6038f2c24ba9206c183dfc5725a14eb65
Your Name <you@example.com>
2021-01-09 09:58:54 +1100

    Restarting the project to a more focused aim

--- plot_input.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-import numpy as np
--- tester.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-import numpy as np
-from matplotlib import rc
-import matplotlib.pyplot as plt
-import dataread as dr
-
-DATA = dr.phantom_evdata('energy_ccc.ev')
