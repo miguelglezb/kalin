d934051c07353964fd0468fe19490fe8ef6b388b
miguelglezb <miguelgb@astro.unam.mx>
2021-02-21 23:24:49 +1100

    Add some tests files (separation*ev and energy*ev) and fix a bug showing column headers more than once

--- main.py
+++ main.py
@@ -11,20 +11,39 @@ import sys
 Filenames = sys.argv[1:]
 
 Data = [dr.phantom_evdata(i,pheaders=False) for i in Filenames]
-Columns = []
 Keys = []
+same_keys = True
 
-#This will change when we mix two diffent TYPE of sets of columns 
+
 
 for d in enumerate(Data):
-    Columns.append({})
+    for k in Data:
+        if k.keys() != d[1].keys():
+            Columns2.append({})
+            same_keys = False
+    
+#This will need to be modified for several types of columns 
+#It will need to make two Column dicts:
+#Columns1 = {[1,2,3...] : Type columns 1 }
+#Columns2 = {[1,2,3...] : Type columns 2 }
+##################################################################
+if same_keys == False:                                           #
+    for d in enumerate(Data):                                    #
+        for i in enumerate(d[1]):                                #
+            print(str(i[0]+1) + ')',i[1])                        #
+            Columns[d[0]].update({str(i[0]): i[1]})              #
+##################################################################
 
 
-for i in enumerate(d[1]):
+#Makes a dict called Columns for the only type of columns in the input files 
+#Columns = {[1,2,3...] : Columns files }
+else:       
+    Columns = {}
+    for i in enumerate(Data[0]):
         print(str(i[0]+1) + ')',i[1])
-        Columns[d[0]].update({str(i[0]): i[1]})
+        Columns.update({str(i[0]): i[1]})
 
-#Columns is [{[1,2,3...]:Columns file 1}, {[1,2,3...]:Columns file 2}...]
+
 
 Ncurves = int(input("Number of curves: "))
 
@@ -34,9 +53,9 @@ print("Put number next to quantity: ")
 for n in range(Ncurves):
     Keys.append(str(int(input("Curve number "+ str(n+1) + ": ")) - 1))
 
+print(Columns)
 for k in Keys:
     for d in Data:
-        for c in Columns:
-            plt.plot(d[c[x_val]], d[c[k]])
+        plt.plot(d[Columns[x_val]], d[Columns[k]])
 
 plt.show() 
